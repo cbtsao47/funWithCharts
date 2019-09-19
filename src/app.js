cd706182e2e386909c0b218c5b30d2fb595084a3
Ben <cbtsao47@gmail.com>
2019-09-18 21:46:59 -0500

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,34 +1,34 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var randomNumber = require('./randomNumber');
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var randomNumber = require("./randomNumber");
 
 var port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-  res.send('Use socket.io-client to connect to the server...');
+app.get("/", function(req, res) {
+  res.send("Use socket.io-client to connect to the server...");
 });
 
-io.on('connection', function (socket) {
-  console.log('connect');
+io.on("connection", function(socket) {
+  console.log("connect");
 
-  var unsubscribe = randomNumber.subscribe(function (number) {
+  var unsubscribe = randomNumber.subscribe(function(number) {
     console.log(number);
 
     var data = {
       value: number,
-      timestamp: Number(new Date()),
+      timestamp: Number(new Date())
     };
 
-    socket.emit('data', data);
+    socket.emit("data", data);
   });
 
-  socket.on('disconnect', function () {
-    console.log('disconnect')
+  socket.on("disconnect", function() {
+    console.log("disconnect");
     unsubscribe();
   });
 });
 
-http.listen(port, function () {
-  console.log('listening on *:' + port);
+http.listen(port, function() {
+  console.log("listening on *:" + port);
 });
