78e00cc5f2119e89a68518c46bcb60adabd3333d
Ben <cbtsao47@gmail.com>
2019-09-19 13:19:18 -0500

    socket connection successful

--- client/src/components/Graphs/Graphs.js
+++ client/src/components/Graphs/Graphs.js
@@ -1,8 +1,24 @@
 import React from "react";
 import LineChart from "./Charts/LineChart";
 import BarChart from "./Charts/BarChart";
-
+import socketIOClient from "socket.io-client";
 class Graphs extends React.Component {
+  state = {
+    data: []
+  };
+  componentDidMount() {
+    let socket = socketIOClient(process.env.REACT_APP_BACKEND_API_URL);
+    /**
+     * listens to 'data' event.
+     * receive randomly generated data from backend.
+     */
+    socket.on("data", data => {
+      this.setState(prevState => ({
+        data: [...prevState.data, data]
+      }));
+    });
+  }
+
   render() {
     return (
       <>
